d0d47a85c8305f0bcf8337ba8c6118bf2405f2b6
rodinis <del.ris@yandex.ru>
2017-12-14 02:30:50 +0300

    Add files via upload

--- main/java/neuro/fin/MainCamera.java
+++ main/java/neuro/fin/MainCamera.java
@@ -2,42 +2,38 @@ package neuro.fin;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.view.View;
 
-import android.hardware.Camera;
-import android.hardware.Camera.Size;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.io.IOException;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.Menu;
-        import android.widget.Button;
-        import android.content.Intent;
-        import android.content.pm.ActivityInfo;
-        import android.content.res.Configuration;
-        import android.view.SurfaceHolder;
-        import android.view.SurfaceView;
-        import android.view.ViewGroup.LayoutParams;
-        import android.view.Window;
-        import android.view.WindowManager;
-
-        import android.hardware.Camera.Size;
-
-        import java.io.File;
-        import java.io.FileOutputStream;
-        import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MainCamera extends Activity implements SurfaceHolder.Callback, View.OnClickListener, Camera.PictureCallback, Camera.PreviewCallback, Camera.AutoFocusCallback
 {
@@ -181,10 +177,66 @@ public class MainCamera extends Activity implements SurfaceHolder.Callback, View
             startActivity(intent);
         }
     }
-
     @Override
     public void onPictureTaken(byte[] paramArrayOfByte, Camera paramCamera)
     {
+        String str = paramArrayOfByte.toString()
+
+
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://simplexapps.ru");
+
+        try {
+                    /*
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(paramArrayOfByte.length);
+            for (int i = 0; i < paramArrayOfByte.length; i++) {
+                nameValuePairs.add(new BasicNameValuePair("type[]", str));
+            }
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs)); */
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("type", str));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        HttpResponse response = null;
+  /*      try {
+            response = httpclient.execute(httppost);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            InputStream instream = null;
+            try {
+                instream = entity.getContent();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                Scanner sc = new Scanner(instream);
+                double value = sc.nextDouble();
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("value", value);
+                startActivity(intent);
+            } finally {
+                try {
+                    instream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } */
+        paramCamera.startPreview();
+    }
+
+    /*@Override
+    public void onPictureTaken(byte[] paramArrayOfByte, Camera paramCamera)
+    {
+
+
         // сохраняем полученные jpg в папке /sdcard/CameraExample/
         // имя файла - System.currentTimeMillis()
 
@@ -207,7 +259,7 @@ public class MainCamera extends Activity implements SurfaceHolder.Callback, View
 
         // после того, как снимок сделан, показ превью отключается. необходимо включить его
         paramCamera.startPreview();
-    }
+    } */
 
     @Override
     public void onAutoFocus(boolean paramBoolean, Camera paramCamera)
